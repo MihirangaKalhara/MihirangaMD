6a94523b28e306213780d63d00877ce57db2e552
ᴍɪʜͥɪʀͣᴀͫɴɢᴀ <89958542+MihirangaKalhara@users.noreply.github.com>
2024-09-25 01:35:39 +0530

    Mihiranga-MD 1.0.0 v (beta) coming Soon

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/87hHVG3/Blue-Futuristic-Illustrative-Artificial-Intelligence-Project-Presentation.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello, /n/nI'm Mihiranga-Multi Device WhatsApp User Bot /n/nDeveloper : Mihiranga Kalhara /n/nThank For Using Mihiranga-Multi Device Bot",
 };
